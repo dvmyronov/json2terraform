20253f22d2fd9b778d61cd2d03ec604d32eaad6f
Dmytro Myronov <dmytro_myronov@epam.com>
2025-03-05 18:37:11 +0200

    JSON to Terraform convertion script

---
+++ json2terraform.sh
@@ -0,0 +1,25 @@
+#jq and sed-based script for json to terraform convertion.
+#Should be pretty fast
+
+#!/bin/bash
+name=$1
+id=$2
+json=$(cat)
+
+#Firstly substitute the 1st line
+echo "$json" | jq . | sed "1s/{/resource \"${name}\" \"${id}\" {/" | sed -n '
+    s/^\([\t ]*\)"\(.*\":\)/\1\2/g;             #Removes the first " from keys
+    s/":/ =/g;                                  #Substitube ": to = to convert JSON key-value pairs to Terraform syntax
+    /{/ {x;s/$/{/;x};                           #If { is found in pattern space, swap hold space with pattern space and append {, then swap back
+    /\[/ {x;s/$/\[/;x};                         #If [ is found in pattern space, swap hold space with pattern space and append [, then swap back             
+    /}/ {x;s/.$//;x};                           #If } is found in pattern space, swap hold space with pattern space, remove the last character, and swap back.
+    /\]/ {x;s/.$//;x};                          #If ] is found in pattern space, swap hold space with pattern space, remove the last character, and swap back.
+    x;                                          #swap hold space and pattern space
+    /{$/ {x;s/,$//;x};                          #if last char is {, swap back, remove trailing comma, and swap again. 
+    x;                                          #swap hold space and pattern space
+    /^.*= \".\{40\}/ {                          #If a value is at least 40 characters long, 
+        s/\(^.*= \)"\(.*\)"/\1<<EOT\n\2\nEOT/g;     #convert it to a heredoc block (<<EOT ... EOT).
+        s/\([${}\`"]\)/\\\1/g;                      #escapes ${}, backticks (`), and double quotes ("`).
+    }
+    p;                                          #print
+'
