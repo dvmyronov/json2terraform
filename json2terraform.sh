249b6138fc2dcaa906b69d5804478338c1d5ad10
Dmytro Myronov <dmytro_myronov@epam.com>
2025-03-23 21:50:30 +0200

    Check for escaped {}[]

--- json2terraform.sh
+++ json2terraform.sh
@@ -10,10 +10,10 @@ json=$(cat)
 echo "$json" | jq . | sed "1s/{/resource \"${name}\" \"${id}\" {/" | sed -n '
     s/^\([\t ]*\)"\(.*\":\)/\1\2/g;             #Removes the first " from keys
     s/":/ =/g;                                  #Substitube ": to = to convert JSON key-value pairs to Terraform syntax
-    /{/ {x;s/$/{/;x};                           #If { is found in pattern space, swap hold space with pattern space and append {, then swap back
-    /\[/ {x;s/$/\[/;x};                         #If [ is found in pattern space, swap hold space with pattern space and append [, then swap back             
-    /}/ {x;s/.$//;x};                           #If } is found in pattern space, swap hold space with pattern space, remove the last character, and swap back.
-    /\]/ {x;s/.$//;x};                          #If ] is found in pattern space, swap hold space with pattern space, remove the last character, and swap back.
+    /{/ {/\\\\{/! {x;s/$/{/;x}};                #If { is found in pattern space; if not escaped by \\; swap hold space with pattern space and append {; then swap back
+    /\[/ {/\\\\\[/! {x;s/$/\[/;x}};             #If [ is found in pattern space; if not escaped by \\; swap hold space with pattern space and append [; then swap back             
+    /}/ {/\\\\}/! {x;s/.$//;x}};                #If } is found in pattern space; if not escaped by \\; swap hold space with pattern space; remove the last character, and swap back.
+    /\]/ {/\\\\\]/! {x;s/.$//;x}};              #If ] is found in pattern space, if not escaped by \\; swap hold space with pattern space, remove the last character, and swap back.
     x;                                          #swap hold space and pattern space
     /{$/ {x;s/,$//;x};                          #if last char is {, swap back, remove trailing comma, and swap again. 
     x;                                          #swap hold space and pattern space
